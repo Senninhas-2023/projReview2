e4fca90c447da20028433fa59ca6535791b48f4f
Sandro Santos [1222645] <1222645@isep.ipp.pt>
2023-09-28 15:11:08 +0100

    removed function sumWithDiscount from calc.ts

--- src/domain/calc.ts
+++ src/domain/calc.ts
@@ -2,7 +2,3 @@
 export function sumOnly(...parcelas: number[]): number {
     return parcelas.reduce((total, parcela) => total + parcela);
 } 
-
-export function sumAndMultiply(...parcelas: number[]): number {
-    return parcelas.reduce((total, parcela) => total + parcela) * parcelas.length;
-}
