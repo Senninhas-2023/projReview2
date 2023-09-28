6c78b802c126cd8bc6738220605b5781dbb6db6c
Sandro Santos [1222645] <1222645@isep.ipp.pt>
2023-09-28 15:06:58 +0100

    Code Changes

--- src/domain/calc.ts
+++ src/domain/calc.ts
@@ -2,3 +2,7 @@
 export function sumOnly(...parcelas: number[]): number {
     return parcelas.reduce((total, parcela) => total + parcela);
 } 
+
+export function sumAndMultiply(...parcelas: number[]): number {
+    return parcelas.reduce((total, parcela) => total + parcela) * parcelas.length;
+}
