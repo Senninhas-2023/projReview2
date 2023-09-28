23016e6a4f4a1cf30e7987ff834909f585be1788
Sandro Santos [1222645] <1222645@isep.ipp.pt>
2023-09-28 15:14:09 +0100

    added function sumWithDiscount to calc.ts

--- src/domain/calc.ts
+++ src/domain/calc.ts
@@ -2,3 +2,7 @@
 export function sumOnly(...parcelas: number[]): number {
     return parcelas.reduce((total, parcela) => total + parcela);
 } 
+
+export function sumWithDiscount(disc: number, ...parcelas: number[]): number {
+    return sumOnly(...parcelas) * (1 - disc);
+}
